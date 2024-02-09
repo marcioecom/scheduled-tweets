f938b0835b40a63ec72a6ff10e567235e33310bd
Márcio Júnior <marcio.mj70@gmail.com>
2024-02-09 16:10:26 -0300

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  get 'about', to: 'about#index'
+
+  root to: 'main#index'
+end
