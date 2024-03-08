312c8eff99865ea98b6328f5a2bf8b15f71ebc84
Márcio Júnior <marcio.mj70@gmail.com>
2024-03-07 23:36:23 -0300

    feat: create job to publish tweet

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -3,4 +3,13 @@ class TwitterAccount < ApplicationRecord
   belongs_to :user
 
   validates :username, uniqueness: true
+
+  def client
+    X::Client.new(
+      api_key: Rails.application.credentials.dig(:twitter, :api_key),
+      api_key_secret: Rails.application.credentials.dig(:twitter, :api_secret),
+      access_token: token,
+      access_token_secret: secret
+    )
+  end
 end
